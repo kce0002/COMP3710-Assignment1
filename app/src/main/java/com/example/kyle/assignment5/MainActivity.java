4dc3c36b5781661342c2cb28ceb46bc5a78dc72b
Kyle Ehlers <kce0002@auburn.edu>
2019-02-05 23:31:05 -0600

    Finished main functionality. TODO's are in comments in MainActivity.java

--- app/src/main/java/com/example/kyle/assignment5/MainActivity.java
+++ app/src/main/java/com/example/kyle/assignment5/MainActivity.java
@@ -1,14 +1,286 @@
+// Kyle Ehlers
+// COMP 3710 - 001
+// 2-5-19
+// Assignment 1
+// TODO: Add decimal formatting
+// TODO: Add log for each text change
+// TODO: Add log for exceptions
+
 package com.example.kyle.assignment5;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText celsiusInput;
+    EditText fahrenheitInput;
+    EditText kmInput;
+    EditText miInput;
+    EditText kgInput;
+    EditText lbInput;
+    EditText literInput;
+    EditText gallonInput;
+
+    boolean tempCheck;
+    boolean distanceCheck;
+    boolean massCheck;
+    boolean volumeCheck;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tempCheck = false;
+        distanceCheck = false;
+        massCheck = false;
+        volumeCheck = false;
+
+        celsiusInput = findViewById(R.id.celsius);
+        celsiusInput.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (tempCheck) {
+                    return;
+                }
+
+                tempCheck = true;
+
+                try {
+                    fahrenheitInput.setText("" + celsiusToFahrenheit(validateInput(celsiusInput.getText().toString())));
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    tempCheck = false;
+                }
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+        });
+
+        fahrenheitInput = findViewById(R.id.fahrenheit);
+        fahrenheitInput.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (tempCheck) {
+                    return;
+                }
+
+                tempCheck = true;
+
+                try {
+                    celsiusInput.setText("" + fahrenheitToCelsius(validateInput(fahrenheitInput.getText().toString())));
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    tempCheck = false;
+                }
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+        });
+
+        kmInput = findViewById(R.id.km);
+        kmInput.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (distanceCheck) {
+                    return;
+                }
+
+                distanceCheck = true;
+
+                try {
+                    miInput.setText("" + kmToMi(validateInput(kmInput.getText().toString())));
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    distanceCheck = false;
+                }
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+        });
+
+        miInput = findViewById(R.id.mi);
+        miInput.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (distanceCheck) {
+                    return;
+                }
+
+                distanceCheck = true;
+
+                try {
+                    kmInput.setText("" + miToKm(validateInput(miInput.getText().toString())));
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    distanceCheck = false;
+                }
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+        });
+
+        kgInput = findViewById(R.id.kg);
+        kgInput.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (massCheck) {
+                    return;
+                }
+
+                massCheck = true;
+
+                try {
+                    lbInput.setText("" + kgToLb(validateInput(kgInput.getText().toString())));
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    massCheck = false;
+                }
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+        });
+
+        lbInput = findViewById(R.id.lb);
+        lbInput.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (massCheck) {
+                    return;
+                }
+
+                massCheck = true;
+
+                try {
+                    kgInput.setText("" + lbToKg(validateInput(lbInput.getText().toString())));
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    massCheck = false;
+                }
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+        });
+
+        literInput = findViewById(R.id.liters);
+        literInput.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (volumeCheck) {
+                    return;
+                }
+
+                volumeCheck = true;
+
+                try {
+                    gallonInput.setText("" + litersToGallons(validateInput(literInput.getText().toString())));
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    volumeCheck = false;
+                }
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+        });
+
+        gallonInput = findViewById(R.id.gallons);
+        gallonInput.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (volumeCheck) {
+                    return;
+                }
+
+                volumeCheck = true;
+
+                try {
+                    literInput.setText("" + gallonsToLiters(validateInput(gallonInput.getText().toString())));
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    volumeCheck = false;
+                }
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+        });
+
     }
 
     public static double celsiusToFahrenheit(double c) {
